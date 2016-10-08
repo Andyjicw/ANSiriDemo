7b6c462a25c707794974f11b10ba6a3fb1453441
andy <479003573@qq.com>
2016-10-08 11:34:25 +0800

    add siri&siriUI

---
+++ ANSiriDemo/Siri/IntentHandler.h
@@ -0,0 +1,13 @@
+//
+//  IntentHandler.h
+//  Siri
+//
+//  Created by andy on 08/10/2016.
+//  Copyright © 2016 AN. All rights reserved.
+//
+
+#import <Intents/Intents.h>
+
+@interface IntentHandler : INExtension
+
+@end
---
+++ ANSiriDemo/SiriUI/IntentViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntentViewController.h
+//  SiriUI
+//
+//  Created by andy on 08/10/2016.
+//  Copyright © 2016 AN. All rights reserved.
+//
+
+#import <IntentsUI/IntentsUI.h>
+
+@interface IntentViewController : UIViewController <INUIHostedViewControlling>
+
+@end
